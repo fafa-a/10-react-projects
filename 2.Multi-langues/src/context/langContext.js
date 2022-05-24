77a71fcfb2942f7cc9e1e829b00565c1147f0950
fafa-a <fabienpuech1@gmail.com>
2022-05-24 16:05:00 +0200

    feat: set navigator language

--- 2.Multi-langues/src/context/langContext.js
+++ 2.Multi-langues/src/context/langContext.js
@@ -1,9 +1,15 @@
 import { createContext, useState } from "react"
 
 export const Context = createContext()
+const supportedLangs = ["EN", "FR", "ES"]
+let browserLang = navigator.language.slice(0, 2).toUpperCase()
 
+if (supportedLangs.indexOf(browserLang) === -1) {
+  console.log("not supported")
+  browserLang = "EN"
+}
 const ContextProvider = ({ children }) => {
-  const [lang, setLang] = useState("EN")
+  const [lang, setLang] = useState(browserLang)
   const ToggleLang = changelang => {
     setLang(changelang)
   }
