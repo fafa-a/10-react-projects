8edaf0089b01c949c41f0f01378659aa34615945
fafa-a <fabienpuech1@gmail.com>
2022-05-24 15:33:41 +0200

    feat: add contenu & toggleLangs

--- 2.Multi-langues/src/App.js
+++ 2.Multi-langues/src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import Contenu from "./Components/Contenu/Contenu"
+import ToggleLangs from "./Components/ToggleLangs/ToggleLangs"
 
 function App() {
   return (
     <>
-
+      <ToggleLangs />
+      <Contenu />
     </>
-  );
+  )
 }
 
 export default App;
