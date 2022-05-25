2b303f882eb4a105ae471ab34c3fe619cd60f14c
fafa-a <fabienpuech1@gmail.com>
2022-05-25 10:52:38 +0200

    feat: set stateanim comp

--- 4.React-animations/src/Pages/StateAnim/StateAnim.js
+++ 4.React-animations/src/Pages/StateAnim/StateAnim.js
@@ -1,6 +1,36 @@
-import React from "react"
+import { useState, useRef, useEffect } from "react"
 import "./StateAnim.css"
+import { useTransition, animated } from "react-spring"
+import { v4 as uuidv4 } from "uuid"
+
+
+
 
 export default function StateAnim() {
-  return <div>StateAnim</div>
+  const [inputData, setInputData] = useState([
+    { id: uuidv4, txt: "Chopin" },
+    { id: uuidv4, txt: "Mozart" },
+    { id: uuidv4, txt: "Bach" },
+  ])
+  const inputRef = useRef()
+  const handleData = e => {
+    e.preventDefault()
+    const newObj = {
+      id: uuidv4,
+      txt: inputRef.current.value,
+    }
+    setInputData([...inputData, newObj])
+    inputRef.current.value = ""
+  }
+  return (
+    <form onSubmit={handleData}>
+      <label htmlFor="piano">Renseignez vos pianiste favoris</label>
+      <input ref={inputRef} type="text" id="piano" />
+      <ul>
+        {inputData.map(item => (
+          <li key={item.id}>{item.txt}</li>
+        ))}
+      </ul>
+    </form>
+  )
 }
