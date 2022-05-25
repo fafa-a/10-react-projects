52c88fabf84005d4defbb91b3fd8c08dcf9f1f66
fafa-a <fabienpuech1@gmail.com>
2022-05-25 10:36:27 +0200

    feat: add list anim

--- 4.React-animations/src/Pages/Liste/Liste.js
+++ 4.React-animations/src/Pages/Liste/Liste.js
@@ -1,5 +1,28 @@
 import React from "react"
 import "./Liste.css"
+import { useTrail, animated } from "react-spring"
+import Card from "../../Components/Card/card"
+import { v4 as uuidv4 } from "uuid"
 export default function Liste() {
-  return <div>Liste</div>
+  const trail = useTrail(9, {
+    from: {
+      opacity: 0,
+      y: -20,
+    },
+    to: {
+      opacity: 1,
+      y: 0,
+    },
+  })
+  return (
+    <div className="list-container">
+      {trail.map((cardStyle, index) => {
+        return (
+          <animated.div key={uuidv4()} style={cardStyle}>
+            <Card />
+          </animated.div>
+        )
+      })}
+    </div>
+  )
 }
