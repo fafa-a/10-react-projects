3291576383afe02dae2e7049f7ea0ef6b3a648b9
fafa-a <fabienpuech1@gmail.com>
2022-05-25 16:41:21 +0200

    feat: add PieChart and set Linechart dynamic & set defaultvalue in option

--- 7.Dashboard/src/Pages/DashboardEmployees/DashboardEmployee.js
+++ 7.Dashboard/src/Pages/DashboardEmployees/DashboardEmployee.js
@@ -1,5 +1,48 @@
-import React from "react"
+import { useContext, useState } from "react"
 import "./DashboardEmployees.css"
+import { DashboardContext } from "../../context/DashboardContext"
+import PieChart from "../../Components/AllChart/PieChart"
+
 export default function DashboardEmployee() {
-  return <div>DashboardEmployee</div>
+  const { dataChart, changeYear, yearData } = useContext(DashboardContext)
+
+  return (
+    <div className="global-container">
+      <h1>Les résultats de l'année: {yearData}</h1>
+      <form>
+        <label htmlFor="year">Choisissez une date</label>
+        <select onChange={changeYear} id="year">
+          <option defaultValue={yearData === "2020" && "2020"} value="2020">
+            2020
+          </option>
+          <option defaultValue={yearData === "2019" && "2019"} value="2019">
+            2019
+          </option>
+          <option defaultValue={yearData === "2018" && "2018"} value="2018">
+            2018
+          </option>
+          <option defaultValue={yearData === "2017" && "2017"} value="2017">
+            2017
+          </option>
+        </select>
+      </form>
+      <h2>Les secteurs qui ont recruté :</h2>
+      <PieChart
+        labels={["Marketing", "Securité", "Front-End"]}
+        data={dataChart.chart5}
+      />
+      <h2>L'âge des nouveaux employés :</h2>
+      <PieChart
+        labels={["< 20", "> 20 && < 30", "> 30 && < 60"]}
+        data={dataChart.chart6}
+      />
+      <h2>Le niveau d'études :</h2>
+      <PieChart
+        labels={["Bac", "> Bac + 2", "> Bac + 5"]}
+        data={dataChart.chart7}
+      />
+      <h2>La nationalité :</h2>
+      <PieChart labels={["USA", "Germany", "UK"]} data={dataChart.chart8} />
+    </div>
+  )
 }
--- 7.Dashboard/src/Pages/DashboardFinance/DashboardFinance.js
+++ 7.Dashboard/src/Pages/DashboardFinance/DashboardFinance.js
@@ -1,7 +1,8 @@
 import { useState, useContext } from "react"
 import "./DashboardFinance.css"
-import BarChart from "../../Components/AllChart/BarChart"
 import { DashboardContext } from "../../context/DashboardContext"
+import BarChart from "../../Components/AllChart/BarChart"
+import LineChart from "../../Components/AllChart/LineChart"
 
 export default function DashboardFinance() {
   const { dataChart, changeYear, yearData } = useContext(DashboardContext)
@@ -12,14 +13,31 @@ export default function DashboardFinance() {
       <form>
         <label htmlFor="year">Choisissez une date</label>
         <select onChange={changeYear} id="year">
-          <option value="2020">2020</option>
-          <option value="2019">2019</option>
-          <option value="2018">2018</option>
-          <option value="2017">2017</option>
+          <option defaultValue={yearData === "2020" && "2020"} value="2020">
+            2020
+          </option>
+          <option defaultValue={yearData === "2019" && "2019"} value="2019">
+            2019
+          </option>
+          <option defaultValue={yearData === "2018" && "2018"} value="2018">
+            2018
+          </option>
+          <option defaultValue={yearData === "2017" && "2017"} value="2017">
+            2017
+          </option>
         </select>
       </form>
       <div className="dashboard-container">
         <BarChart data={dataChart.chart1} name={"Chiffres Bimestriels"} />
+        <LineChart
+          data={dataChart.chart2}
+          name={"Nombres d'abonnés (en milliers)"}
+        />
+        <BarChart data={dataChart.chart4} name={"Budget marketing"} />
+        <LineChart
+          data={dataChart.chart3}
+          name={"Nombres de clients (en milliers)"}
+        />
       </div>
     </div>
   )
