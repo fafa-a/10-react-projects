7df31409253ff5573d9c25e1e666ec321d400208
fafa-a <fabienpuech1@gmail.com>
2022-05-29 18:43:30 +0200

    feat: create ui & card

--- 9.Ecommerce/src/Pages/ProductShowcase/ProductShowcase.js
+++ 9.Ecommerce/src/Pages/ProductShowcase/ProductShowcase.js
@@ -1,5 +1,23 @@
-import React from "react"
+import { useState, useEffect, useRef } from "react"
+import "./ProductShowcase.css"
+import { useParams } from "react-router-dom"
+import inventory from "../../data/inventory"
+import { useDispatch } from "react-redux"
 
 export default function ProductShowcase() {
-  return <div>ProductShowcas</div>
+  const { id } = useParams()
+  const productClicked = inventory.findIndex(
+    obj => obj.title.replace(/\s/g, "").trim() === id
+  )
+  return (
+    <div className="showcase">
+      <div className="container-img-showcase">
+        <img
+          src={`/images/${inventory[productClicked].img}.png`}
+          alt=""
+          className="img-showcase"
+        />
+      </div>
+    </div>
+  )
 }
