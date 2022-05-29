9709a50454f3a4a0b81c977d0d388d2b995c909d
fafa-a <fabienpuech1@gmail.com>
2022-05-29 16:51:51 +0200

    feat: log out

--- 8.auth-react-cours/src/components/Navbar.js
+++ 8.auth-react-cours/src/components/Navbar.js
@@ -1,10 +1,21 @@
 import { useContext } from "react"
 import { Link } from "react-router-dom"
 import { UserContext } from "../context/userContext"
+import { signOut } from "firebase/auth"
+import { auth } from "../firebase-config"
+import { useNavigate } from "react-router-dom"
 
 export default function Navbar() {
   const { toggleModals } = useContext(UserContext)
+  const navigate = useNavigate()
 
+  const logOut = async () => {
+    try {
+      await signOut(auth)
+      navigate("/")
+    } catch (error) {}
+    alert("For some reason you are not logged out")
+  }
   return (
     <nav className="navbar navbar-light bg-light px-4">
       <Link to="/">AuthJS</Link>
@@ -21,7 +32,9 @@ export default function Navbar() {
         >
           Sign in
         </button>
-        <button className="btn btn-danger ms-2">Log out</button>
+        <button onClick={logOut} className="btn btn-danger ms-2">
+          Log out
+        </button>
       </div>
     </nav>
   )
--- 8.auth-react-cours/src/pages/Home.js
+++ 8.auth-react-cours/src/pages/Home.js
@@ -1,9 +1,14 @@
-import React from "react"
+import { useContext } from "react"
+import { UserContext } from "../context/userContext"
 
 export default function Home() {
+  const { currentUser } = useContext(UserContext)
+
   return (
     <div className="container p-5">
-      <h1 className="display-3 text-light">Hi, sign up or sign in</h1>
+      <h1 className="display-3 text-light">
+        {currentUser ? "Welcome" : "Hi, sign up or sign in"}
+      </h1>
     </div>
   )
 }
