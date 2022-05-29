fb3006c313a06a37209920093039da927563102d
fafa-a <fabienpuech1@gmail.com>
2022-05-29 19:41:52 +0200

    feat: update item

--- 9.Ecommerce/src/redux/reducer/cartReducer.js
+++ 9.Ecommerce/src/redux/reducer/cartReducer.js
@@ -1,3 +1,4 @@
+/* eslint-disable default-case */
 const INITIAL_STATE = {
   cart: [],
 }
@@ -5,18 +6,28 @@ const INITIAL_STATE = {
 export default function cartReducer(state = INITIAL_STATE, action) {
   switch (action.type) {
     case "ADDITEM":
-    const indexItemAdd= state.cart.findIndex(obj => obj.id === action.payload.id)
-    if (indexItemAdd !== -1) {
-
-    }else{
-      const newArr = [...state.cart]
-      newArr.push(action.payload)
-      console.log(newArr)
-      return{
-        cart: newArr
+      const indexItemAdd = state.cart.findIndex(
+        obj => obj.id === action.payload.id
+      )
+      if (indexItemAdd !== -1) {
+        const updatedQuantity = {
+          ...state.cart[indexItemAdd],
+          quantity: state.cart[indexItemAdd].quantity + action.payload.quantity,
+        }
+        const newArr = [...state.cart]
+        newArr.splice(indexItemAdd, 1, updatedQuantity)
+        console.log(newArr)
+        return {
+          cart: newArr,
+        }
+      } else {
+        const newArr = [...state.cart]
+        newArr.push(action.payload)
+        return {
+          cart: newArr,
+        }
       }
-    }
-    break
+      break
     case "UPDATEITEM":
       return {}
   }
